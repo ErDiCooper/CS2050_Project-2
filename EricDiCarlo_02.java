ad7dd48d1de212b4c91a6df1986995a56abfdc81
ErDiCooper <ericdicarlo@hotmail.com>
2019-06-11 18:06:13 -0600

    Fixed some variable declarations and processGradeItemData.

--- EricDiCarlo_02.java
+++ EricDiCarlo_02.java
@@ -4,24 +4,16 @@ import java.io.*;
 
 public class EricDiCarlo_02 {
 
-   private Student student1;
-   private Student student2;
+   private static Student student1;
+   private static Student student2;
       
-   private GradeItem gradeItem1;
-   private GradeItem gradeItem2;
-      
-   private final String INPUT_FILENAME = "Project_02_Input01.txt";
+   private static GradeItem gradeItem1;
+   private static GradeItem gradeItem2;
+   
 
    public static void main(String[] args) {   
    
-      Student student1;
-      Student student2;
-      
-      GradeItem gradeItem1;
-      GradeItem gradeItem2;
-      
       final String INPUT_FILENAME = "Project_02_Input01.txt";
-      
       
       System.out.println("Running Test 1a:");
       System.out.println("Running Test 1b:");
@@ -33,26 +25,27 @@ public class EricDiCarlo_02 {
       System.out.println("Running Test 2a:");
       System.out.println("Running Test 2b:");
       
+      
       System.out.println("\n*******************************************************************\n");
       
       System.out.println("Running Test 3a:");
       System.out.println("Running Test 3b:");
-   
-   
-   
+      /*
+      if (gradeItem1.equals(gradeItem2)) {
+         System.out.println("GradeItem objects are equal: Student IDs are" + GradeItem1.id);
+      }
+      else {
+         System.out.println("GradeItem objects are unequal: Student IDs are " + GradeItem1.id +
+                            " and " + GradeItem2.id);
+      }   
+      */
    }
    
-   public void proccessStudentData(String[] data) {
+   public static void proccessStudentData(String[] data) {
       // STUB STUB STUB
    }
    
-   public void proccessGradeItemData(String[] data) {
-      if(!data[0].equals("GRADE ITEM") || !data[1].equals("ADD")) {
-         throw new IllegalArgumentException("This function is only meant to add new Grade Items." +
-                                            " Please try again with the correct file input.");
-      }
-      
-      gradeItem1 = new GradeItem(data[4], data[3], data[5], data[6],
-                                 data[7], data[8], data[9]);
+   public static void proccessGradeItemData(GradeItem grade, String[] data) {    
+      grade = new GradeItem(data[3], data[2], data[4], data[5], data[6], data[7], data[8]);
    }
 }
